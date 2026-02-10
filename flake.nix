b5e2975e181e43a88f1f2c3a408de56cfc67acc3
Luca Fondo <luca.fondo@trai.ch>
2026-02-10 17:47:13 +0100

    Introduce nix flake devshell

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Development environment for GitOps";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            opentofu
+            kubectl
+            kubectx
+            fluxcd
+            sops
+
+            nixfmt-tree
+            nixfmt
+            nixd
+          ];
+        };
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
